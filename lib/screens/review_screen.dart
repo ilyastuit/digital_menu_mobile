cd0cc3bb5b7b3a9af1cf8f7851566bd87ca1a92d
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2020-12-05 16:48:02 +0500

    edited review screen

--- lib/screens/review_screen.dart
+++ lib/screens/review_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reviews_slider/reviews_slider.dart';
 
 class ReviewScreen extends StatefulWidget {
   @override
@@ -6,8 +7,55 @@ class ReviewScreen extends StatefulWidget {
 }
 
 class _ReviewScreenState extends State<ReviewScreen> {
+  int selectedValue1;
+  int selectedValue2;
+
+  void onChange1(int value) {
+    setState(() {
+      selectedValue1 = value;
+    });
+  }
+
+  void onChange2(int value) {
+    setState(() {
+      selectedValue2 = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Meal quality?',
+              style: TextStyle(color: Color(0xFF6f7478), fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ReviewSlider(
+              onChange: onChange1,
+            ),
+            Text(selectedValue1.toString()),
+            SizedBox(height: 20),
+            Text(
+              'Price?',
+              style: TextStyle(color: Color(0xFF6f7478), fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ReviewSlider(
+                optionStyle: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+                onChange: onChange2,
+                initialValue: 1,
+                options: ['Terrible', 'Bad', 'OK', 'Good', 'Excellent']),
+            Text(selectedValue2.toString()),
+          ],
+        ),
+      ),
+    );
   }
 }
